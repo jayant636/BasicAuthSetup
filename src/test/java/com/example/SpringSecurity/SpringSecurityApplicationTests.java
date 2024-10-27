6b47edd6a43cafa07d4ef9c0e48d3cd32e993961
jayant636 <jayantjalandra@gmail.com>
2024-10-27 17:08:32 +0530

    JWT Implemented

--- src/test/java/com/example/SpringSecurity/SpringSecurityApplicationTests.java
+++ src/test/java/com/example/SpringSecurity/SpringSecurityApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.SpringSecurity;
 
+import com.example.SpringSecurity.entity.UserEntity;
+import com.example.SpringSecurity.service.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringSecurityApplicationTests {
 
+	@Autowired
+	private JwtService jwtService;
+
 	@Test
 	void contextLoads() {
+		UserEntity userEntity = new UserEntity(4l,"jay@gmail.com","1234");
+
+		String token = jwtService.generateToken(userEntity);
+
+		System.out.println(token);
+
+		Long userId = jwtService.getUserIdFromToken(token);
+
+		System.out.println("UserId"+userId);
 	}
 
 }
